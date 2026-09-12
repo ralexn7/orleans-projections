7d70d031b71cc45d9394b508c622cdd0ec5ab601
Alexander Ryzhkov <aryzhkov@dmvdesk.com>
2026-09-12 20:43:11 +0300

    Support method call node

---
+++ Orleans.Projections/Serialization/MethodCallNode.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+
+namespace Orleans.Projections.Serialization;
+
+public record MethodCallNode (MethodDescriptor Method, List<INode> Parameters) : INode
+{
+    public Expression BuildExpression (ParameterExpression parameter)
+    {
+        return Expression.Call(Method.Resolve(), Parameters.Select(p => p.BuildExpression(parameter)));
+    }
+}
---
+++ Orleans.Projections/Serialization/MethodDescriptor.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+
+namespace Orleans.Projections.Serialization;
+
+[GenerateSerializer]
+public record MethodDescriptor (string DeclaringType, string Name, string[] ParameterTypes, string[] GenericArguments)
+{
+    public static MethodDescriptor Create (MethodInfo method)
+    {                                
+        var definition = method.IsGenericMethod ? method.GetGenericMethodDefinition() : method;
+
+        return new MethodDescriptor(DeclaringType: method.DeclaringType!.AssemblyQualifiedName!, Name: method.Name,
+            ParameterTypes: [..definition.GetParameters().Select(p => p.ParameterType.AssemblyQualifiedName ?? p.ParameterType.FullName!)],
+            GenericArguments: method.IsGenericMethod
+                ? [..method.GetGenericArguments().Select(t => t.AssemblyQualifiedName!)]
+                : []);
+    }
+
+    public MethodInfo Resolve ()
+    {
+        var declaringType = Type.GetType(DeclaringType, throwOnError: true)!;
+        var parameterTypes = ParameterTypes.Select(t => Type.GetType(t, true)!).ToArray();
+
+        if (GenericArguments.Length == 0)
+        {        
+            return declaringType.GetMethod(Name, BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance,
+                       binder: null, types: parameterTypes, modifiers: null) ??
+                   throw new MissingMethodException(declaringType.FullName, Name);
+        }
+                                                           
+        var genericArgs = GenericArguments.Select(t => Type.GetType(t, true)!).ToArray();
+
+        var definition = declaringType.GetMethods(BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance)
+            .Where(m => m.Name == Name && m.IsGenericMethodDefinition)
+            .Where(m => m.GetGenericArguments().Length == genericArgs.Length)
+            .First(m => m.GetParameters()
+                .Select(p => p.ParameterType.Name)
+                .SequenceEqual(ParameterTypes.Select(SimpleName)));
+
+        return definition.MakeGenericMethod(genericArgs);
+    }
+
+    private static string SimpleName(string assemblyQualified) =>
+        Type.GetType(assemblyQualified, false)?.Name ?? assemblyQualified;
+}
